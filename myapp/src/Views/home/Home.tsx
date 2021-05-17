3015f4584aaa4d3328836882f6a6999095a891a2
Orzoch <47740876+kziel445@users.noreply.github.com>
2021-05-17 16:29:12 +0200

    10.05 exercises

--- myapp/src/Views/home/Home.tsx
+++ myapp/src/Views/home/Home.tsx
@@ -1,13 +1,30 @@
 import React from 'react';
 
 import NavPanel from '../../Components/NavPanel';
+import http from '../../utils/http';
+import movieService from '../../services/movies.service';
+const Home = () => {
+    const [search, setSearch] = React.useState('harry potter');
+    const [movie, setMovie] = React.useState({});
 
-const Home =()=>{
-    return (
-        <div><NavPanel/>
+    React.useEffect(() => {
+        const makeRequest = async () => {
+            try {
+
+            const responce = movieService.searchByName(search,1);
+            
+            setMovie(responce);
+            }catch(error){
+                console.log(error);
+            }
+        };
+        makeRequest();
+    }, [search]);
+   return (
+        <div> 
+            <NavPanel />
             <div>This is home page</div>
         </div>
     );
-
 }
 export default Home;
---
+++ myapp/src/key.ts
@@ -0,0 +1,1 @@
+export const apikey = 33fcce66
